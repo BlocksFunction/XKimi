985a8fd15e4a115deade686c357498a4665ac4df
BlocksFunction <3398817447@qq.com>
2025-04-23 18:04:22 +0800

    Update Code

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+	<div class="bg-white w-screen h-screen">
+		<div class="" data-tauri-drag-region><button>a</button></div>
+		<RouterView />
+	</div>
+</template>
+
+<style scoped></style>
---
+++ src/views/ChatView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template><p>a</p></template>
+
+<style scoped></style>
